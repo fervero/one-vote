1cfa1100a654cc347ede4d88e462defe1b9a1566
Maciej Bojko <maciej.bojko@gmail.com>
2019-05-23 20:10:01 +0200

    Imported and parsed csv. Kinda.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as Papa from 'papaparse';
+import { results } from './2015-gl-lis-okr.js';
+
+const parser = csvString =>
+  new Promise(resolve => {
+    Papa.parse(results, {
+      complete: parsed => {
+        const relevant = parsed.data.map(row => row.slice(25));
+        resolve(relevant);
+      },
+    });
+  });
+
+parser(results).then(console.table);
 
 function App() {
   return (
@@ -8,7 +22,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save.
         </p>
         <a
           className="App-link"
