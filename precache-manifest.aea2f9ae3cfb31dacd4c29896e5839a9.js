f08d493cb858f06afa753153d285a7996ea26154
Maciej Bojko <maciej.bojko@gmail.com>
2019-09-22 14:18:15 +0200

    Updates

---
+++ precache-manifest.aea2f9ae3cfb31dacd4c29896e5839a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ced50cb0b83c29847b8b94bb3b22489c",
+    "url": "/one-vote/index.html"
+  },
+  {
+    "revision": "9d5bf0ab7cc5a1c86e7e",
+    "url": "/one-vote/static/css/main.42dcc1d7.chunk.css"
+  },
+  {
+    "revision": "f2e3958c77288203eafe",
+    "url": "/one-vote/static/js/2.34b5113d.chunk.js"
+  },
+  {
+    "revision": "9d5bf0ab7cc5a1c86e7e",
+    "url": "/one-vote/static/js/main.e824877c.chunk.js"
+  },
+  {
+    "revision": "614d16c9998baec65210",
+    "url": "/one-vote/static/js/runtime~main.e49002bf.js"
+  }
+]);
